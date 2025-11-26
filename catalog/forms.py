06bc9533de2b0c92a9b1a33354d4f646e8a04fb6
Yaryna Shcherban <yaryna.shcherban@gmail.com>
2025-11-26 22:16:16 +0200

    added html files

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,10 +1,26 @@
 from django import forms
-from store.models import Book, Publisher, Author
+from store.models import Book, Publisher, Author, Genre
 
 class BookForm(forms.ModelForm):
+    genres = forms.ModelMultipleChoiceField(
+        queryset=Genre.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
     class Meta:
         model = Book
-        fields = ['name', 'isbn', 'price', 'publisher', 'author']
+        fields = ['name', 'isbn', 'price', 'publisher', 'author', 'genres']
         widgets = {
             'author': forms.CheckboxSelectMultiple(),
+            'publisher': forms.Select(),
+            'genres': forms.CheckboxSelectMultiple(),
         }
+
+    def save(self, commit=True):
+        book = super().save(commit=False)
+        if commit:
+            book.save()
+            self.save_m2m()
+            book.genres.set(self.cleaned_data['genres'])
+        return book
