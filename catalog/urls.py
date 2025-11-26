f41df32a5fc974c70d557aef4ff55a3f1c789f9a
Yaryna Shcherban <yaryna.shcherban@gmail.com>
2025-11-26 19:51:35 +0200

    created catalog and modified settings and urls in bookstore_progect

---
+++ catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = "catalog"
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('create/', views.book_create, name='book_create'),
+    path('<int:book_id>/', views.book_detail, name='book_detail'),
+    path('<int:book_id>/edit/', views.book_update, name='book_update'),
+    path('<int:book_id>/delete/', views.book_delete, name='book_delete'),
+]
---
+++ catalog/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from store.models import Book
+from forms import BookForm
