f1c27535a06ff5cc0d69f3a0468c667997bf6fd5
YarynkaShcherban <yaryna.shcherban@gmail.com>
2025-11-28 09:29:11 +0200

    Revert "modified html files"
    This reverts commit 0f08504fbe15be7489fa55d895149e295143670f.

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,33 +2,20 @@ from django import forms
 from store.models import Book, Publisher, Author, Genre
 
 class BookForm(forms.ModelForm):
-    author = forms.ModelMultipleChoiceField(
-        queryset=Author.objects.all(),
-        widget=forms.CheckboxSelectMultiple(),
-        required=False,
-        label='Автори',
-    )
-
     genres = forms.ModelMultipleChoiceField(
         queryset=Genre.objects.all(),
-        widget=forms.CheckboxSelectMultiple(),
-        required=False,
-        label='Жанри'
+        widget=forms.CheckboxSelectMultiple,
+        required=False
     )
 
     class Meta:
         model = Book
         fields = ['name', 'isbn', 'price', 'publisher', 'author', 'genres']
         widgets = {
+            'author': forms.CheckboxSelectMultiple(),
             'publisher': forms.Select(),
+            'genres': forms.CheckboxSelectMultiple(),
         }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['author'].label_from_instance = lambda obj: f"{obj.first_name} {obj.last_name}"
-        self.fields['genres'].label_from_instance = lambda obj: obj.name
-        if self.instance and self.instance.pk:
-            self.fields['genres'].initial = self.instance.genres.values_list('pk', flat=True)
 
     def save(self, commit=True):
         book = super().save(commit=False)
