74191f653498e4b511d7ccf6a94bec9f57066521
YarynkaShcherban <yaryna.shcherban@gmail.com>
2025-11-28 09:29:11 +0200

    Revert "added html files"
    This reverts commit 06bc9533de2b0c92a9b1a33354d4f646e8a04fb6.

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- catalog/forms.py
+++ catalog/forms.py
@@ -1,26 +1,10 @@
 from django import forms
-from store.models import Book, Publisher, Author, Genre
+from store.models import Book, Publisher, Author
 
 class BookForm(forms.ModelForm):
-    genres = forms.ModelMultipleChoiceField(
-        queryset=Genre.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=False
-    )
-
     class Meta:
         model = Book
-        fields = ['name', 'isbn', 'price', 'publisher', 'author', 'genres']
+        fields = ['name', 'isbn', 'price', 'publisher', 'author']
         widgets = {
             'author': forms.CheckboxSelectMultiple(),
-            'publisher': forms.Select(),
-            'genres': forms.CheckboxSelectMultiple(),
         }
-
-    def save(self, commit=True):
-        book = super().save(commit=False)
-        if commit:
-            book.save()
-            self.save_m2m()
-            book.genres.set(self.cleaned_data['genres'])
-        return book
