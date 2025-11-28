7090dbfe1febb3b735cab682a88a9d02d8a89ee9
YarynkaShcherban <yaryna.shcherban@gmail.com>
2025-11-28 09:29:11 +0200

    Revert "created catalog and modified settings and urls in bookstore_progect"
    This reverts commit f41df32a5fc974c70d557aef4ff55a3f1c789f9a.

--- catalog/views.py
+++
@@ -1,3 +0,0 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from store.models import Book
-from forms import BookForm
--- store/repositories/base_repo.py
+++ store/repositories/base_repo.py
@@ -6,19 +6,19 @@ class BaseRepo(ABC):
     def __init__(self, model: Model):
         self.model = model
 
-    def get_all(self): 
+    def get_all(self):  # вичитка всіх даних
         return self.model.objects.all()
 
-    def get_by_id(self, _id):
+    def get_by_id(self, _id):  # пошук по ID
         try:
             return self.model.objects.get(pk=_id)
         except self.model.DoesNotExist:
             return None
 
-    def create(self, **kwargs):
+    def create(self, **kwargs):  # створення нового екземпляру
         return self.model.objects.create(**kwargs)
 
-    def update(self, _id, **kwargs):
+    def update(self, _id, **kwargs): # Оновлення існуючого запису
         instance = self.get_by_id(_id)
         if not instance:
             return None
@@ -27,18 +27,18 @@ class BaseRepo(ABC):
         instance.save()
         return instance
 
-    def delete(self, _id):
+    def delete(self, _id): # Видалення запису
         instance = self.get_by_id(_id)
         if not instance:
             return False
         instance.delete()
         return True
 
-    def filter_by(self, **kwargs): 
+    def filter_by(self, **kwargs):  # Пошук за фільтрами
         return self.model.objects.filter(**kwargs)
 
-    def get_first(self, **kwargs): 
+    def get_first(self, **kwargs):  # Пошук першого збігу
         return self.model.objects.filter(**kwargs).first()
 
-    def count(self): 
+    def count(self):  # Підрахунок кількості записів
         return self.model.objects.count()
